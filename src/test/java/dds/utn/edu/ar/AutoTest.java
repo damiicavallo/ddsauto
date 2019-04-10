e1b6252e9614529e7b0c1ad247cc78f71bf1f98f
damiicavallo <damian.cavallo94@gmail.com>
2019-04-10 11:51:54 -0300

    agregando auto test

---
+++ src/test/java/dds/utn/edu/ar/AutoTest.java
@@ -0,0 +1,16 @@
+package dds.utn.edu.ar;
+
+import org.junit.Test;
+
+public class AutoTest {
+
+    @Test
+    public void testEncendidoOk(){
+
+        Auto auto = new Auto();
+        auto.setCombustible(14.0);
+
+
+    }
+
+}
